d41f78ed21157e8e4296c91edfc734f74feeac45
sumit-kharat-05 <sumitkharat686@gmail.com>
2026-07-22 10:25:29 +0530

    Array

---
+++ array.js
@@ -0,0 +1,47 @@
+//Array//
+
+let arr1 = ["html", "css", "tailwind css", "js", "react js"];
+let arr2 = [100, 200, 300, 400, 500];
+let arr3 = [45,78,78,9,23,45,85,21,10,20,45];
+for (let i = 0; i < arr1.length; i++) {
+  console.log(arr1[i]);
+}
+console.log("Array Length =", arr1.length);
+console.log("Push Element =", arr1.push("vue js"));
+console.log(arr1);
+console.log("Pop Element =", arr1.pop("vue js"));
+console.log(arr1);
+console.log("Add Element At 0th Index =", arr1.unshift("http"));
+console.log(arr1);
+console.log("Remove Element At 0th Index =", arr1.shift("http"));
+console.log(arr1);
+console.log("Convert Array To String =", arr2.toString());
+console.log(arr2);
+console.log("Connect Two Arrays =", arr1.concat(arr2));
+console.log("Return Splice Elements =", arr1.slice(1,2));
+console.log(arr1);
+console.log("Return Specific Elements =" ,arr1.splice(1,3));
+console.log(arr1);
+console.log("Check This Elements Are Include Or Not =",arr1.includes('html'));
+console.log("Check Index Of Element =",arr1.indexOf('react js'));
+console.log("Reverse Array =",arr1.reverse());
+console.log(arr1);
+console.log("Sorted Array =", arr3.sort());
+console.log(arr3);
+
+function returnPosition()
+{
+    let pos = event.target.value;
+    arr1.splice(1,4);
+    console.log(arr1);
+}
+
+
+
+
+
+
+
+
+
+
